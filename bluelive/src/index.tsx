4a9aa3dc7038c95c194654090c1b1c40f18f5594
lucaskahlil <kahlilpereira@gmail.com>
2023-01-20 14:28:29 -0300

    implement route

--- bluelive/src/index.tsx
+++ bluelive/src/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Login } from './components/pages/login/login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/pages/home/home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/home" element={<Home />}/>
+      </Routes >
+    </BrowserRouter>
   </React.StrictMode>
 );
 
