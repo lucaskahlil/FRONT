69f172158aff63c56752ecdd18d3cc47014e5f88
lucaskahlil <kahlilpereira@gmail.com>
2023-01-20 12:47:42 -0300

    form login implementado

--- bluelive/src/components/moleculas/login-form/login-form.tsx
+++ bluelive/src/components/moleculas/login-form/login-form.tsx
@@ -1,12 +1,24 @@
+import { FormEvent } from "react";
+
 export function LoginForm() {
+  
+  function handleSubmit (e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const LoginPayload = {
+      email: e.currentTarget.email.value,
+      senha: e.currentTarget.senha.value,
+    };
+    console.log(LoginPayload)
+  }
+
   return (
     <div>
       <h2>BlueLive</h2>
-      <form>
-       <input />
-       <input />
+      <form onSubmit={handleSubmit}>
+       <input placeholder="Seu e-mail" name="email"/>
+       <input placeholder="Sua senha" name="senha"/>
+       <button type="submit">Login</button>
       </form>
-      <button>Login</button>
     </div>
   );
 }
