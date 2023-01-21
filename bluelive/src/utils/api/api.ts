c4c6203bdfd4848271733ad65fcdf7a4b431ae0b
Lucas Kahlil <kahlilpereira@gmail.com>
2023-01-20 21:51:13 -0300

    correção interceptor

--- bluelive/src/utils/api/api.ts
+++ bluelive/src/utils/api/api.ts
@@ -1,16 +1,32 @@
+import axios from "axios";
 import { LoginRequest } from "../types/request";
 
+axios.defaults.baseURL = "https://xbox-live-api.onrender.com"
+axios.defaults.headers.post["Content-Type"] = "application/json"
+
+axios.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("token")
+    if (token) {
+      config.headers = {
+        Authorization: "Bearer " + token,
+      };
+    }
+    return config;
+  }, 
+  function (error) {
+    return Promise.reject(error);
+  }
+)
+
 export const api = {
-  login: async ({Email, Password} :LoginRequest) => {
-    const response = await fetch(
-      "https://xbox-live-api.onrender.com/auth",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ Email, Password }),
-      });
-      return await response.json(); 
+  login: async ({ Email, Password }: LoginRequest) => {
+    try {
+      const response = await axios.post('/auth', { Email, Password })
+      localStorage.setItem("token", response.data.token);
+      return response.data;
+    } catch (err) {
+      alert(err);
+    }
   },
 };
